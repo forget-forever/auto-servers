c9804f632b5e4e4fe0c16faaf517d5f16f477251
zml <hehezml@qq.com>
2022-01-06 21:28:37 +0800

    加入请求获取

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 import { program } from "commander";
+import typeofJsonc from "typeof-jsonc";
 
 program.version(require("../package.json").version, '-v --version').usage('<command> [name] <option>');
 
-
+console.log(typeofJsonc(`{
+  // 测试
+  a: ''
+}`, 'test', {export: true, singleLineJsDocComments: true}))
 
 program.parse(process.argv);
---
+++ src/utils/request.ts
@@ -0,0 +1,5 @@
+import request from "request"
+
+export const fetch = () => {
+  return request('')
+}
