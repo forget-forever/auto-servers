0b0732ec080ee283c868940f223e855c534f24ed
zml <hehezml@qq.com>
2022-01-07 19:15:53 +0800

    增加转json字符串的测试

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ import typeofJsonc from "typeof-jsonc";
 
 program.version(require("../package.json").version, '-v --version').usage('<command> [name] <option>');
 
-console.log(typeofJsonc(`{
-  // 测试
-  a: ''
-}`, 'test', {export: true, singleLineJsDocComments: true}))
+console.log(typeofJsonc(`{// 测试
+  a: ''}`, 'test', {export: true, singleLineJsDocComments: true}))
 
 program.parse(process.argv);
